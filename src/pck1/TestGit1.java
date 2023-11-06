78c90bf9ec97c65111a34372d0b3d0dcf72d08bb
musthaqahamed <musthaq53ahamed@gmail.com>
2023-11-06 17:23:52 +0530

    commit 1

--- src/pck1/TestGit1.java
+++ src/pck1/TestGit1.java
@@ -5,6 +5,8 @@ public class TestGit1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Git Community");
+		//new line 1
+		//new line 2
 	}
 
 }
