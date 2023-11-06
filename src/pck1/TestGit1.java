2eb8057b9b4d53f208c5843ca47274020caa3232
musthaqahamed <musthaq53ahamed@gmail.com>
2023-11-06 17:26:07 +0530

    commit 2

--- src/pck1/TestGit1.java
+++ src/pck1/TestGit1.java
@@ -2,11 +2,31 @@ package pck1;
 
 public class TestGit1 {
 
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Git Community");
 		//new line 1
 		//new line 2
+		
 	}
 
 }
